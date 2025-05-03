f9c258839bbec8025a30fc58f9f9ac9b73b91d61
Auguste Dubuisson <guy32807@comcast.net>
2025-05-02 22:04:35 -0400

    Performed some housekeeping tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import { GlobalStyles } from './styles/GlobalStyles';
